4ba53b6a270de20532206390337b1b11c4d864e5
Igor Khristiuk <ikhristiuk@gmail.com>
2021-09-13 09:12:21 +0300

    initial commit

---
+++ src/main/java/org/example/http/framework/Request.java
@@ -0,0 +1,19 @@
+package org.example.http.framework;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.List;
+import java.util.Map;
+
+@Value
+@Builder
+public class Request {
+  String method;
+  String path;
+  Map<String, String> headers;
+  Map<String,List<String>> query;
+  Map<String, List<String>> form;
+  @Builder.Default
+  byte[] body = new byte[]{};
+}
